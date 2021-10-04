9c42c89e4771d543500f37639865c1b8dec8aecb
Sylvain Mazet <sylvain.mazet@softbridge.fr>
2021-10-04 16:07:29 -0400

    * move to corporate package

--- src/main/java/com/softbridge/neo4j/Last.java
+++ src/main/java/com/softbridge/neo4j/Last.java
@@ -1,4 +1,4 @@
-package example;
+package com.softbridge.neo4j;
 
 import org.neo4j.procedure.*;
 
--- src/main/java/com/softbridge/neo4j/TraverseDemo.java
+++ src/main/java/com/softbridge/neo4j/TraverseDemo.java
@@ -1,4 +1,4 @@
-package example;
+package com.softbridge.neo4j;
 
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
